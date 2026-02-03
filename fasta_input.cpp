ecd56d65e13639df77b2955d053b3577cf8857af
rohleet <rohithakkankol1234@gmail.com>
2026-02-03 18:37:35 +0530

    Logic for fasta input is improved and optimized and made simple according to the how characters are written in fasta file

--- fasta_input.cpp
+++ fasta_input.cpp
@@ -3,6 +3,10 @@
 using namespace std;
 using namespace std::filesystem;
 
+unordered_set<char> nitrogen_base = {'A', 'C', 'T', 'G'};
+
+unordered_set<char> nitrogen_base_lower_case = {'a', 'c', 't', 'g'};
+
 dna_profile fasta_input::file_input(path s) {
 
     if(!exists(s)){
@@ -15,41 +19,65 @@ dna_profile fasta_input::file_input(path s) {
 
     ifstream ip_file(s);
 
-    string input = "";
+    // string input = "";
 
-    bool first = false;
+    char input;
 
-    string header;
+    bool individual_detail = false;
+
+    string header = "";
     string line;
 
-    while(getline(ip_file,line)){
-        if(!first){
-            header = line;
-            first = 1;
-        } else 
-            for(unsigned char c : line){
-
-                if(c>='0' && c<='9'){
-                    cerr<<"Rejected : corrupted dna sequence, contains number";
-                    exit(-1);
-                }
-
-                std::transform(line.begin(), line.end(), line.begin(),
-                    [](unsigned char c) {
-                    return std::toupper(c);
-                });
-
+    while(ip_file.get(input)){
+        if(!individual_detail){
+            if(input=='\n'){
+                individual_detail = 1;
+                continue;
             }
-
-            input+=line;
-
+            header+=input;
+        } else {
+            if(input == '\n') {
+                continue;
+            }
+            if(nitrogen_base.find(input)!=nitrogen_base.end()){
+                line+=input;
+            } else if(nitrogen_base_lower_case.find(input)!=nitrogen_base_lower_case.end()){
+                line+=static_cast<char>(toupper(static_cast<unsigned char>(input)));
+            } else {
+                cerr<<"Rejected : corrupted dna sequence, contains number or special characters";
+                exit(-1);
+            } 
+        }
     }
 
-    if(input.length()==0){
+    // while(getline(ip_file,line)){
+    //     if(!individual_detail){
+    //         header = line;
+    //         individual_detail = 1;
+    //     } else 
+    //         for(unsigned char c : line){
+
+    //             if(c>='0' && c<='9'){
+    //                 cerr<<"Rejected : corrupted dna sequence, contains number";
+    //                 exit(-1);
+    //             }
+
+    //             std::transform(line.begin(), line.end(), line.begin(),
+    //                 [](unsigned char c) {
+    //                 return std::toupper(c);
+    //             });
+
+    //         }
+
+    //         input+=line;
+
+    // }
+
+    if(line.length()==0){
         cerr<<"Error file doesn't contain dna sequence";
         exit(-2);
     }
 
-    return dna_profile(header,input);
+    return dna_profile(header,line);
 
 }
--- fasta_input.h
+++ fasta_input.h
@@ -6,11 +6,14 @@
 #include<iostream>
 #include<cctype>
 #include<algorithm>
+#include<unordered_set>
 
 
 class fasta_input {
 
     public :
         
+        static std::unordered_set<int> nitrogen_base;
+        static std::unordered_set<int> nitrogen_base_lower_case;
         static dna_profile file_input(std::filesystem::path s);
 };
